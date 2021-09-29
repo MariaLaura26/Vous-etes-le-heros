24a62d82748184bac9aea7a8029725e71bc9c1a0
MariaLaura26 <90852162+MariaLaura26@users.noreply.github.com>
2021-09-29 10:50:09 -0400

    changements

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-cdd
+const Princess = {
+    subtitle:'Princess kidnapée',
+    text:'La princess a été kidnapée',
+    img:'assets/img/princess_kidnapee.png',
+    options:["la sauver, rien faire"]
+};
+const chaptersObj
