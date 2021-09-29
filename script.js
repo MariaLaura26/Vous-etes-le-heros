7f666f9520045d9e02afe2597b5328f988208b83
MariaLaura26 <90852162+MariaLaura26@users.noreply.github.com>
2021-09-29 11:02:42 -0400

    changements

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const Princess = {
+const Reveil = {
+    subtitle:'Mario se lève',
+    text:'Mario se lève et entend à la radio que la princess a été kidnapée',
+    img:'',
+    options:["continuer"]
+};
+const chaptersObj = {
     subtitle:'Princess kidnapée',
     text:'La princess a été kidnapée',
     img:'assets/img/princess_kidnapee.png',
     options:["la sauver, rien faire"]
-};
-const chaptersObj
+}
