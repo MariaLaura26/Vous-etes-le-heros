68e4e0227866e9a0bc922eaa8568ca1a3c4fbbde
MariaLaura26 <90852162+MariaLaura26@users.noreply.github.com>
2021-09-29 11:41:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-const Reveil = {
-    subtitle:'Mario se lève',
-    text:'Mario se lève et entend à la radio que la princess a été kidnapée',
+
+let chaptersObj= {
+     le_reveil: {
+        subtitle:'Mario se lève',
+        text:'Mario se lève et entend à la radio que la princess a été kidnapée',
+        img:'assets/img/mario_se_leve',
+        options:["continuer"],
+},
+ la_princess:{
+     subtitle:'',
+     text:'',
+     img:'',
+     options:[""],
+
+},
+demande_laide:{
+    subtitle:'',
+    text:'',
     img:'',
-    options:["continuer"]
+    options:[""],
+},
+lac_rempli:{
+    subtitle:'',
+    text:'',
+    img:'',
+    options:["nager,ils prennent le bateau qui est à côté"],
+},
+bateau_qui_coule:{
+    subtitle:'',
+    text:'',
+    img:'',
+    options:[""],
+},
+les_sauve:{
+    subtitle:'',
+    text:'',
+    img:'',
+    options:[""],
+},
+la_foret:{
+    subtitle:'',
+    text:'',
+    img:'',
+    options:[""],
+},
+ils_trouevent_la_cachette:{
+    subtitle:'',
+    text:'',
+    img:'',
+    options:[""],
+},
+avez_vous_les_cles:{
+    subtitle:'',
+    text:'',
+    img:'',
+    options:[""],
+},
+princess_sauve:{
+    subtitle:'',
+    text:'',
+    img:'',
+    options:[""],
+},
 };
-const chaptersObj = {
-    subtitle:'Princess kidnapée',
-    text:'La princess a été kidnapée',
-    img:'assets/img/princess_kidnapee.png',
-    options:["la sauver, rien faire"]
-}
