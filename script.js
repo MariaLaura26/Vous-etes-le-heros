a9d8ae1baf8297c54c486ad31ef21c5e38078e5f
MariaLaura26 <coronelpetitmarialaura@gmail.com>
2021-09-30 08:58:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,39 @@ let chaptersObj = {
         subtitle:'Mario se lève',
         text:'Mario se lève et entend à la radio que la princess a été kidnapée',
         img:"https://github.com/MariaLaura26/Vous-etes-le-heros/blob/8be926cafaaa7c33e25333f17e34f6a629279e9e/assets/img/mario_se_leve.jpg",
-        options: ["continuer"],
+        options: [
+            (continuer = {
+                text:"Continuer",
+                action:"goToChapter('la_princess')",
+
+            }),
+        ],
 },
  la_princess:{
      subtitle:'Princess kidnapée',
      text:"La princess à été kidnapée, tu es le seul espoir qu'il reste",
      img:"https://github.com/MariaLaura26/Vous-etes-le-heros/blob/8be926cafaaa7c33e25333f17e34f6a629279e9e/assets/img/princess_kidnapee.png",
-     options:["rien faire, la sauver"],
+     options:[
+         (rienfaire = {
+             text:"Rien faire",
+             action:"goToChapter('mangez_par')"
+         }),
+         (lasauver={
+             text:"Sauver la princess",
+             action:"goToChapter('demande_laide')",
+         })
+        ],
 
+},
+mangez_par:{
+    subtitle:"Vous avez été mangé par un animal sauvage",
+    text:"",
+    img:"",
+    options:[
+        (continuer = {
+            action:"goToChapitre('le_reveil')"
+        }
+        )],
 },
 demande_laide:{
     subtitle:"Il demande de l'aide à son frère",
