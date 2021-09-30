a92c637f7fe935593dcc6f4e506cceae3009c0d8
MariaLaura26 <coronelpetitmarialaura@gmail.com>
2021-09-29 20:58:17 -0400

    img

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let chaptersObj = {
         subtitle:'Mario se lève',
         text:'Mario se lève et entend à la radio que la princess a été kidnapée',
         img:"https://github.com/MariaLaura26/Vous-etes-le-heros/blob/8be926cafaaa7c33e25333f17e34f6a629279e9e/assets/img/mario_se_leve.jpg",
-        options:["continuer"],
+        options: ["continuer"],
 },
  la_princess:{
      subtitle:'Princess kidnapée',
@@ -13,13 +13,7 @@ let chaptersObj = {
      options:["rien faire, la sauver"],
 
 },
-rien_faire:{
-    subtitle:'Mangé par une bête sauvage',
-    text:'',
-    img:'https://github.com/MariaLaura26/Vous-etes-le-heros/blob/d80359ded36a6d0e45c477bd40645c9a49cf7569/assets/img/1611257680_super-mario-3d-world-bowsers-fury.jpg',
-    options:["début du jeu"],
 
-},
 demande_laide:{
     subtitle:"Il demande de l'aide à son frère",
     text:"Mario ne peut pas reussir cette mision tout seul alors il demande de l'aide à son frère",
@@ -32,46 +26,34 @@ lac_rempli:{
     img:'https://github.com/MariaLaura26/Vous-etes-le-heros/blob/8d8ba2753719a1f024e73a475df16848b9dd5c16/assets/img/10759678.jpg',
     options:["Nager,ils prennent le bateau qui est à côté"],
 },
-le_bateau_coule:{
-    subtitle:'',
-    text:'',
-    img:'https://github.com/MariaLaura26/Vous-etes-le-heros/blob/c7fe2f3f2569df3c0f6b20b142fc22d96dd10476/assets/img/istockphoto-1257129389-170667a.jpg',
-    options:[""],
-},
-na_ger:{
-subtitle:'',
-text:'',
-img:'',
-options:['']
-},
 les_sauve:{
     subtitle:'',
     text:'',
-    img:'',
+    img:'https://github.com/MariaLaura26/Vous-etes-le-heros/blob/3c8981f71b27b2271817a7eb3d08100942c0a06b/assets/img/film-super-mario-bros-sortie-2022-e1549100004926.jpg',
     options:[""],
 },
 la_foret:{
     subtitle:'',
     text:'',
-    img:'',
-    options: [""],
+    img:'https://github.com/MariaLaura26/Vous-etes-le-heros/blob/881c370b1479e92a3d132566321c1c147b5d31f9/assets/img/mario-bros.jpg',
+    options: ["ils abandonnent,continuet à marcher"],
 },
 ils_trouevent_la_cachette:{
     subtitle:'',
     text:'',
-    img:'',
+    img:'https://github.com/MariaLaura26/Vous-etes-le-heros/blob/33b177e65d7529d972e8e604a97fccbf315a5d08/assets/img/the_super_mario_bros._super_show.jpg',
     options: [""],
 },
 avez_vous_les_cles:{
     subtitle:'',
     text:'',
-    img:'',
-    options:[""],
+    img:'https://github.com/MariaLaura26/Vous-etes-le-heros/blob/33b177e65d7529d972e8e604a97fccbf315a5d08/assets/img/67029641-dessin-d-or-en-forme-de-cl%C3%A9-coeur-illustration-vectorielle-eps-10.jpg',
+    options:["non,oui"],
 },
 princess_sauve:{
     subtitle:'',
     text:'',
-    img:'',
+    img:'https://github.com/MariaLaura26/Vous-etes-le-heros/blob/640d618735559fca2c392704d1a0e14ef3f23424/assets/img/52069d36f025bf8f0400207857adbd41.jpg',
     options:[""],
 },
 };
