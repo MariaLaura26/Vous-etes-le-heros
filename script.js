d4601e08cbaba45d8ab6e69624e8793eddfc650a
MariaLaura26 <90852162+MariaLaura26@users.noreply.github.com>
2021-10-05 14:22:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -185,6 +185,7 @@ princess_sauve:{
 };
 
 function goToChapter(chapterName) { 
+    const chapitre = chapterName
 console.log(chaptersObj[chapterName].subtitle);
 console.log(chaptersObj[chapterName].text);
 }
